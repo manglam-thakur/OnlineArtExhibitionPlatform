b5f9105535e8e306279c47df56aa0f14a4227692
manglam-thakur <thakurmanglam7771@gmail.com>
2024-11-19 15:33:31 +0530

    Update LikeDAO.java

--- src/main/java/com/onlineart/exhibition/dao/LikeDAO.java
+++ src/main/java/com/onlineart/exhibition/dao/LikeDAO.java
@@ -1,5 +1,12 @@
 package com.onlineart.exhibition.dao;
 
-public class LikeDAO {
+import com.onlineart.exhibition.model.Like;
+import java.util.List;
 
+public interface LikeDAO {
+    void save(Like like);
+    void delete(Long likeId);
+    List<Like> findLikesByUser(Long userId);
+    List<Like> findLikesByArtwork(Long artworkId);
+    boolean isLiked(Long userId, Long artworkId);  // Check if user has liked a specific artwork
 }
